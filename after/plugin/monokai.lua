86286e566449a05f31855d7005dff8403fde4f06
Simon Prinsen <simon360@hotmail.nl>
2024-07-12 23:20:40 +0200

    nvim config

---
+++ after/plugin/monokai.lua
@@ -0,0 +1,10 @@
+--Color scheme
+require('monokai').setup { palette = require('monokai').soda }
+
+--transparant background
+function SetBackground(color)
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+end
+
+SetBackground()
