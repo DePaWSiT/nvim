f37a62a4e76a48f267b10b46fbd0714f593e8b63
Simon Prinsen <simon360@hotmail.nl>
2024-07-15 18:12:17 +0200

    another update through neovim git, this is getting strange

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ks', function()
 	builtin.grep_string({search = vim.fn.input("grep > ")});
 end)
