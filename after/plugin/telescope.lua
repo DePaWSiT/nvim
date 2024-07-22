ee059576a36c35da30683c3383c6faf3efe3cad7
Simon Prinsen <simon360@hotmail.nl>
2024-07-22 15:02:23 +0200

    update to config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
+require('DePaWSiT.remap')
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ks', function()
+vim.keymap.set('n', TELESCOPE_FIND_FILES, builtin.find_files, {})
+vim.keymap.set('n', TELESCOPE_GIT_SEARCH, builtin.git_files, {})
+vim.keymap.set('n', TELESCOPE_GREP_SEARCH, function()
 	builtin.grep_string({search = vim.fn.input("grep > ")});
 end)
 
