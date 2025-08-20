932e4a4f56635e79d44833df6cf3bd275a26c27d
Simon <simon360@hotmail.nl>
2025-08-20 10:42:59 +0200

    auto-fetch on startup, non-blocking

--- lua/plugins/fugitive_spec.lua
+++ lua/plugins/fugitive_spec.lua
@@ -1,3 +1,30 @@
 return {
-	"tpope/vim-fugitive"
+	"tpope/vim-fugitive",
+	event = "VeryLazy",
+	dependancies = {"nvim-lua/plenary.nvim"},
+	config = function()
+		local Job = require("plenary.job")
+
+		Job:new({
+			command = "git",
+			args = { "fetch" },
+			on_start = function()
+				print("Starting git fetch...")
+			end,
+			on_exit = function(j, return_val)
+				if return_val == 0 then
+					print("git fetch completed successfully")
+				else
+					print("git fetch failed with exit code:", return_val)
+					print("stderr:", table.concat(j:stderr_result(), "\n"))
+				end
+			end,
+			on_stdout = function(_, line)
+				print("stdout:", line)
+			end,
+			on_stderr = function(_, line)
+				print("stderr:", line)
+			end,
+		}):start()
+	end,
 }
