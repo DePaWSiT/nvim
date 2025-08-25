e8750f5c672a88b4a6a7ce8a2663dd648753c6af
Simon <simon360@hotmail.nl>
2025-08-25 14:24:36 +0200

    added support for dev of dotnet

--- lua/plugins/easy_dotnet_spec.lua
+++ lua/plugins/easy_dotnet_spec.lua
@@ -1,7 +1,7 @@
 return {
 	"GustavEikaas/easy-dotnet.nvim",
 	ft = { "cs", "rzls", "sln", "csproj" },
-	--dir="~/projects/easy-dotnet.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
 	opts = {},
+	dev = false,
 }
