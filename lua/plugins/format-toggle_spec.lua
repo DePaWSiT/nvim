87a4edee61fdba2c693f322c9802e8f304ee738f
Simon Prinsen <simon360@hotmail.nl>
2026-07-07 23:17:00 +0200

    update format-toggle for lazy loading

--- lua/plugins/format-toggle_spec.lua
+++ lua/plugins/format-toggle_spec.lua
@@ -1,5 +1,8 @@
 return {
   "DePaWSiT/format-toggle.nvim",
+  lazy = true,
+  event = { "BufRead", "BufNewFile" },
+  cmd = { "FormatToggle" },
   -- dev = true,
   -- dir = "~/Documents/format-toggle.nvim/",
   opts = {
