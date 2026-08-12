c371b45168fe950ca94809881ebf50c398df3908
DePaWSiT <simon360@hotmail.nl>
2026-08-12 17:54:15 +0200

    .

--- lua/plugins/dadbod_spec.lua
+++ lua/plugins/dadbod_spec.lua
@@ -6,6 +6,10 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     lazy = true,
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
     cmd = {
       "DBUI",
       "DBUIToggle",
